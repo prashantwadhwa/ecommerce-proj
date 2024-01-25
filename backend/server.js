3c46592a529c2aecebaff1785dadd86c05fd562d
prashantwadhwa <prashantwadhwa19@gmail.com>
2024-01-25 13:02:21 +0530

    pathUpdated

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(cors());
 
 app.use("/api/products", productRoutes);
@@ -33,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
