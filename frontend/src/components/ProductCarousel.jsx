a56f584ebde3b1e8914a8d23b305fe167a6db6fc
Prashant Wadhwa <77116676+prashantwadhwa@users.noreply.github.com>
2024-02-17 15:04:36 +0530

    Update ProductCarousel.jsx

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -18,7 +18,7 @@ const ProductCarousel = () => {
       {products.map(product => (
         <Carousel.Item key={product._id}>
           <Link to={`product/65ab7f6ba00ee82db443e546`}>
-            <Image src="https://uc7ba8c8df86834e318672a1948b.previews.dropboxusercontent.com/p/thumb/ACJ9Z-UYN3WUv2sTJ5aiBQfDZTNc6B582_AYnDl28wJBmga8wkmlFffLU7cNLkt-T8bRJ-c_VaR8XixVPOxAsu4rJswOUMzzMQR0Dn4Ec7C6aUQT-w11JfKMpnY310JPPEzA_Quc300fw-BWX8b6RoRUi14avH2CiPd5IWBopSjwtPA6ljYPz2IC5etUYKMIXIrggcUjVB-yAGQ2SirgNcC9k7AfB9YGpfMOSmgpkKYD2nU_9N-v9ksIBxmlZQqkSigb7dxnEBP-v5mkvlD4EzVjrjizVDafFHE0DqH3b-CZ2Qrv7vVjejsx6Qr-jv7Xf0ZO1ZpkulK1TemQKQLtTQN1giU_bix5U-6RNWNJtY-5l_aa-3A-r_xlaMFA3RZ5f3g/p.png"/>
+            <Image src="https://www.dropbox.com/scl/fi/4bhdrepjq3rbyoj9bqcrb/Frame-8.png?rlkey=bzvczgbcs3h3wx5wcg27w06np&dl=0"/>
             {/* <Carousel.Caption className="carousel-caption">
               <h2>
                 {product.name} (Rs {product.price})
